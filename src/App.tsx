eee8baca24e0b56344e83b78db9e87978b001995
Nastassia27 <anastasia.derugo@gmail.com>
2023-05-24 10:06:42 +0300

    add max value

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Counter} from "./components/counter";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [counter, setCounter] = useState<number>(0)
+const maxValue=5
+    const changeCounter = () => {
+        counter = counter + 1;
+        setCounter(counter)
+    }
+    const resetCounter = () => {
+        setCounter(0)
+    }
+
+    return (
+        <div>
+            <Counter
+                counter={changeCounter}
+                number={counter}
+                reset={resetCounter}
+                maxValue={maxValue}
+            />
+        </div>
+    );
 }
 
 export default App;

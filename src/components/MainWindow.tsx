d29069c76f3b953b9d03000e4911e9957b363d7b
Nastassia27 <anastasia.derugo@gmail.com>
2023-05-31 10:31:40 +0300

    change conditions

--- src/components/MainWindow.tsx
+++ src/components/MainWindow.tsx
@@ -1,25 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from "./counter.module.css";
 import {Button} from "./Button";
 
 type PropsType = {
-    counter: () => void;
-    number: number;
-    reset: () => void;
+    counter: number
     maxValue: number
+    minValue: number
+    changeCounter: () => void
+    resetCounter: () => void
+    buttonClicked: boolean
+    previousMaxValue: number
+    previousMinValue: number
 }
 export const MainWindow = (props: PropsType) => {
+
+
     return (
-        <div className={s.wrapper}>
-            <div className={s.counter}>
-                <div className={props.number === props.maxValue ? s.numberError : s.number}>
-                    {props.number}
-                </div>
-                <div className={s.buttons}>
-                    <Button title={'inc'} callBack={props.counter} disabled={props.number === props.maxValue}/>
-                    <Button title={'reset'} callBack={props.reset} disabled={props.number === 0}/>
-                </div>
+
+        <div className={s.counter}>
+            <div className={props.counter === props.previousMaxValue ? s.numberError : s.number}>
+                {
+                    (!props.buttonClicked) && (props.minValue < 0 || props.maxValue < 0 || props.maxValue <= props.minValue)
+                        ? <label className={s.incorrectNumber} htmlFor="counter">Incorrect value</label>
+                        : (
+                            (!props.buttonClicked)
+                                ? (
+                                    <label className={s.firstMessage} htmlFor="counter">Enter values and press "Set"</label>
+                                ) : props.counter)
+                }
             </div>
+            <div className={s.buttons}>
+                <Button className={s.click} title={'inc'} callBack={props.changeCounter}
+                        disabled={props.counter === props.previousMaxValue || !props.buttonClicked || props.minValue < 0 || props.maxValue < 0 || props.maxValue <= props.minValue}/>
+                <Button className={s.click} title={'reset'} callBack={props.resetCounter}
+                        disabled={props.counter === props.previousMinValue || !props.buttonClicked}/>
+            </div>
+
         </div>
     )
 };
--- src/components/SetValues.tsx
+++ src/components/SetValues.tsx
@@ -1,30 +1,33 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import s from "./counter.module.css";
 import {Button} from "./Button";
+import Input from "./Input";
 
 type PropsTypes = {
-    counter: ()=> void
+    maxValue: number
+    minValue: number
+    changeMax:(event: ChangeEvent<HTMLInputElement>)=>void
+    changeMin:(event: ChangeEvent<HTMLInputElement>)=>void
+    setValue:()=>void
 }
 
 export const SetValues = (props: PropsTypes) => {
+
     return (
-        <div className={s.wrapper}>
-            <div className={s.counter}>
-                <div className={s.inputgroup}>
-                    <label htmlFor="maxValue">Макс</label>
-                    <input type="number" id="maxValue"/>
-                </div>
-                <div className={s.inputgroup}>
-                    <label htmlFor="minValue">Мин</label>
-                    <input type="number" id="minValue"/>
-                </div>
-                <div className={s.buttons}>
-                    <Button title={'Set'} callBack={props.counter}/>
-                </div>
+        <div className={s.counter}>
+            <div className={s.inputgroup}>
+                <label htmlFor="maxValue">Max Value</label>
+                <Input class={props.maxValue<0 || props.maxValue<=props.minValue ? s.inputError: ''} type={'number'} id={'maxValue'} value={props.maxValue} callback={props.changeMax}/>
+            </div>
+            <div className={s.inputgroup}>
+                <label htmlFor="minValue">Min Value</label>
+                <Input class={props.minValue<0 || props.maxValue<=props.minValue ? s.inputError: ''} type={'number'} id={'minValue'} value={props.minValue} callback={props.changeMin}/>
+            </div>
+            <div className={s.buttons}>
+                <Button disabled={props.minValue<0 || props.maxValue<0 || props.maxValue<=props.minValue} className={s.click} title={'Set'} callBack={props.setValue}/>
             </div>
         </div>
-    )
-
+    );
 };
 
 export default SetValues;

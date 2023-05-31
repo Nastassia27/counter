6b65078bab8c7bc5f7f4d9c9d1a41b84bdaf2c9e
Nastassia27 <anastasia.derugo@gmail.com>
2023-05-31 21:34:41 +0300

    useEffect for localstorage

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,18 +1,55 @@
-import React, {ChangeEvent, useState} from "react";
+import React, {ChangeEvent, useEffect, useState} from "react";
 import s from './counter.module.css'
 import {Button} from "./Button";
 import SetValues from "./SetValues";
 import MainWindow from "./MainWindow";
 
-type PropsType = {}
 
-export function Counter(props: PropsType) {
-    let [maxValue, setMaxValue] = useState<number>(0)
-    let [minValue, setMinValue] = useState<number>(0)
-    let [counter, setCounter] = useState<number>(0)
+export function Counter() {
+    let [maxValue, setMaxValue] = useState<number>(() => {
+        const storedValue = localStorage.getItem('Max value');
+        return storedValue ? JSON.parse(storedValue) : 1;
+    });
+    let [minValue, setMinValue] = useState<number>(() => {
+        const storedValue = localStorage.getItem('Min value');
+        return storedValue ? JSON.parse(storedValue) : 0;
+    })
+    let [counter, setCounter] = useState<number>(() => {
+        const storedValue = localStorage.getItem('Counter');
+        return storedValue ? JSON.parse(storedValue) : 0;
+    })
     let [buttonClicked, setButtonClicked] = useState<boolean>(false)
     let [previousMinValue, setPreviousMinValue] = useState(0);
     let [previousMaxValue, setPreviousMaxValue] = useState(0);
+
+   /* useEffect(() => {
+        let maxValueAsString = localStorage.getItem('Max value')
+        let minValueAsString = localStorage.getItem('Min value')
+        let newCounter = localStorage.getItem('Counter')
+debugger
+        if (maxValueAsString) {
+            let newMaxValue = JSON.parse(maxValueAsString)
+            setMaxValue(newMaxValue)
+        }
+        debugger
+        if (minValueAsString) {
+            let newMinValue = JSON.parse(minValueAsString)
+            setMinValue(newMinValue)
+        }
+        debugger
+        if (newCounter) {
+            let newNewCounter = JSON.parse(newCounter)
+            setCounter(newNewCounter)
+        }
+    }, []) //без зависимостей, значит будет отрабатывать каждый раз при перезагрузке*/
+
+
+    useEffect(() => {
+        localStorage.setItem('Min value', JSON.stringify(minValue))
+        localStorage.setItem('Max value', JSON.stringify(maxValue))
+        localStorage.setItem('Counter', JSON.stringify(counter))
+    }, [minValue, maxValue, counter])
+
     const onChangeMax = (event: ChangeEvent<HTMLInputElement>) => {
         setMaxValue(Number(event.target.value))
         setButtonClicked(false)
@@ -23,19 +60,19 @@ export function Counter(props: PropsType) {
     }
     const changeCounter = () => {
 
-       if(counter<previousMaxValue)
-        setCounter(counter+1)
+        if (counter < previousMaxValue)
+            setCounter(counter + 1)
     }
     const resetCounter = () => {
         setCounter(previousMinValue);
     }
     const setValue = () => {
+        setPreviousMinValue(minValue)
+        setPreviousMaxValue(maxValue)
         setButtonClicked(true)
         setCounter(minValue)
-        setPreviousMinValue(minValue);
-        setPreviousMaxValue(maxValue)
-    }
 
+    }
     return (
         <div className={s.wrapper}>
             <div>

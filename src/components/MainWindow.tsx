06d76d860093869d391c7bffed6e39a5f6123081
Nastassia27 <anastasia.derugo@gmail.com>
2023-06-01 20:58:12 +0300

    fix conditions

--- src/components/MainWindow.tsx
+++ src/components/MainWindow.tsx
@@ -13,21 +13,22 @@ type PropsType = {
     previousMinValue: number
 }
 export const MainWindow = (props: PropsType) => {
+const labelVariant = ()=>{
+    if(!props.buttonClicked && (props.minValue < 0 || props.maxValue < 0 || props.maxValue <= props.minValue)){
+        return <label className={s.incorrectNumber} htmlFor="counter">Incorrect value</label>
+    } else if (!props.buttonClicked){
+        return  <label className={s.firstMessage} htmlFor="counter">Enter values and press "Set"</label>
+    } else{
+        return <p>{props.counter}</p>
+    }
 
+}
 
     return (
 
         <div className={s.counter}>
             <div className={props.counter === props.previousMaxValue ? s.numberError : s.number}>
-                {
-                    (!props.buttonClicked) && (props.minValue < 0 || props.maxValue < 0 || props.maxValue <= props.minValue)
-                        ? <label className={s.incorrectNumber} htmlFor="counter">Incorrect value</label>
-                        : (
-                            (!props.buttonClicked)
-                                ? (
-                                    <label className={s.firstMessage} htmlFor="counter">Enter values and press "Set"</label>
-                                ) : props.counter)
-                }
+                { labelVariant()}
             </div>
             <div className={s.buttons}>
                 <Button className={s.click} title={'inc'} callBack={props.changeCounter}
